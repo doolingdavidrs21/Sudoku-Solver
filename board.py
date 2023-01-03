7b7b74d42b9ce1894f3c7531732a2a247b1d5469
H Suhas Prabhu <85097320+Suhas-2002@users.noreply.github.com>
2023-01-03 23:07:46 +0530

    Update board.py

--- board.py
+++ board.py
@@ -1,7 +1,26 @@
 import pandas as pd
 import numpy as np
 
-normal_board = """ \t\t\t\t\t\t\t\t\t\t---------------------------------
+four_board = """\t\t\t\t\t\t\t\t\t\t\t-----------------
+\t\t\t\t\t\t\t\t\t\t\t|  0  0  |  0  0  |  
+\t\t\t\t\t\t\t\t\t\t\t|  0  0  |  0  0  |  
+\t\t\t\t\t\t\t\t\t\t\t-----------------
+\t\t\t\t\t\t\t\t\t\t\t|  0  0  |  0  0  |  
+\t\t\t\t\t\t\t\t\t\t\t|  0  0  |  0  0  | 
+\t\t\t\t\t\t\t\t\t\t\t-----------------"""
+
+six_board = """\t\t\t\t\t\t\t\t\t\t\t----------------------
+\t\t\t\t\t\t\t\t\t\t\t|  0  0  0  |  0  0  0  |  
+\t\t\t\t\t\t\t\t\t\t\t|  0  0  0  |  0  0  0  |  
+\t\t\t\t\t\t\t\t\t\t\t----------------------
+\t\t\t\t\t\t\t\t\t\t\t|  0  0  0  |  0  0  0  |  
+\t\t\t\t\t\t\t\t\t\t\t|  0  0  0  |  0  0  0  | 
+\t\t\t\t\t\t\t\t\t\t\t----------------------
+\t\t\t\t\t\t\t\t\t\t\t|  0  0  0  |  0  0  0  | 
+\t\t\t\t\t\t\t\t\t\t\t|  0  0  0  |  0  0  0  |  
+\t\t\t\t\t\t\t\t\t\t\t----------------------"""
+
+nine_board = """ \t\t\t\t\t\t\t\t\t\t---------------------------------
 \t\t\t\t\t\t\t\t\t\t|  0  0  0  |  0  0  0  |  0  0  0  |
 \t\t\t\t\t\t\t\t\t\t|  0  0  0  |  0  0  0  |  0  0  0  |
 \t\t\t\t\t\t\t\t\t\t|  0  0  0  |  0  0  0  |  0  0  0  |
@@ -15,19 +34,7 @@ normal_board = """ \t\t\t\t\t\t\t\t\t\t---------------------------------
 \t\t\t\t\t\t\t\t\t\t|  0  0  0  |  0  0  0  |  0  0  0  |
 \t\t\t\t\t\t\t\t\t\t---------------------------------"""
 
-six_board = """\t\t\t\t\t\t\t\t\t\t\t----------------------
-\t\t\t\t\t\t\t\t\t\t\t|  0  0  0  |  0  0  0  |  
-\t\t\t\t\t\t\t\t\t\t\t|  0  0  0  |  0  0  0  |  
-\t\t\t\t\t\t\t\t\t\t\t----------------------
-\t\t\t\t\t\t\t\t\t\t\t|  0  0  0  |  0  0  0  |  
-\t\t\t\t\t\t\t\t\t\t\t|  0  0  0  |  0  0  0  | 
-\t\t\t\t\t\t\t\t\t\t\t----------------------
-\t\t\t\t\t\t\t\t\t\t\t|  0  0  0  |  0  0  0  | 
-\t\t\t\t\t\t\t\t\t\t\t|  0  0  0  |  0  0  0  |  
-\t\t\t\t\t\t\t\t\t\t\t----------------------"""
-
-
-def matrix_to_df(matrix,M):
-    df = pd.DataFrame(matrix, columns=np.arange(1, M+1))
-    df.index = np.arange(1, len(df) + 1)
+def matrix_to_df(matrix,n):
+    df = pd.DataFrame(matrix, columns=np.arange(1, n+1))
+    df.index = np.arange(1, n+1)
     return df
